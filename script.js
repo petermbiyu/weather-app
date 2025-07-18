b824f056fe0eaed5149b7f52c00a5d28ffe7b1c3
peter Mbiyu <petersmasha@gmail.com>
2025-07-18 15:23:17 +0300

    App is working..

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
-const cityName = document.getElementById(city);
-const btn = document.querySelector(".btn");
-
 const apiKey = "fb3333adb488be9e61a9f3b0aeb7413a";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=kisumu&appid=${apiKey}&units=metric`;
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
-document.addEventListener("click", async () => {
-  await weatherCall();
-});
+const cityName = document.querySelector(".search input");
+const btn = document.querySelector(".search button");
+const weatherImg = document.querySelector(".weather-image");
+const weather = document.querySelector(".weather")
 
-const weatherCall = async () => {
-  const apiCall = await fetch(apiUrl);
-  //   if (!apiCall.ok) {
-  //     throw new Error(Response.status);
-  //   }
-  const data = await apiCall.json();
-  console.log(data);
 
-  document.getElementsByClassName("city")[0].innerHTML = data.name;
-  document.getElementsByClassName("temp")[0].innerHTML =
-    data.main.temp + "<sup>o</sup>C";
-  document.getElementsByClassName("humidity")[0].innerHTML =
+const weatherCall = async (city) => {
+  //fetch data from the google map API
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  const data = await response.json();
+
+//use the data to dynamically change the values of the selected city
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp)
+     + "<sup>o</sup>C";
+  document.querySelector(".humidity").innerHTML =
     data.main.humidity + "%";
-  document.getElementsByClassName("wind")[0].innerHTML =
+  document.querySelector(".wind").innerHTML =
     data.wind.speed + "Km/H";
+
+     //change the weather image based on the weather data. 
+    switch(data.weather[0].main) {
+      case "Clouds":
+        weatherImg.src = "./images/clouds.png";
+        break;
+      case "Clear":
+        weatherImg.src = "./images/clear.png";
+        break;
+      case "Rain":
+        weatherImg.src = "./images/rain.png";
+        break;
+      case "Drizzle":
+        weatherImg.src = "./images/drizzle.png";
+        break;
+      case "Mist":
+        weatherImg.src = "./images/mist.png";
+        break;
+      default:
+        weatherImg.src = "./images/rain.png";
+        
+    }  
+    //change the value of the style to display the weather information
+    weather.style.display = "block";
 };
+
+
+//respond to the search button on the home page
+//pass the value of the input to the api
+btn.addEventListener("click", () => {
+  weatherCall(cityName.value);
+
+
+
+  
+});
