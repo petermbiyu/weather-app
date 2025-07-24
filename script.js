4a6b9e9d592a7e47f41650dc3f572e1c897b6e07
peter Mbiyu <petersmasha@gmail.com>
2025-07-24 11:51:06 +0300

    updated the weather app to handle error

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 const cityName = document.querySelector(".search input");
 const btn = document.querySelector(".search button");
 const weatherImg = document.querySelector(".weather-image");
-const weather = document.querySelector(".weather")
+const weather = document.querySelector(".weather");
+const errDiv = document.querySelector(".err");
+const errMessage = document.querySelector(".errMessage");
 
 
 const weatherCall = async (city) => {
   //fetch data from the google map API
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   const data = await response.json();
-
+  console.log(data);
+  if(data.cod == 404) {
+errMessage.innerHTML = data.message;
+errDiv.style.display = "block";
+weather.style.display = "none";
+  } else if (data.cod == 200) {
 //use the data to dynamically change the values of the selected city
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp)
@@ -43,7 +50,12 @@ const weatherCall = async (city) => {
         
     }  
     //change the value of the style to display the weather information
+    errDiv.style.display = "none";
     weather.style.display = "block";
+  }
+    
+
+
 };
 
 
